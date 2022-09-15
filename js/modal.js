35e623c84c9420493fddb829bf363c25b912d4d1
JuanCelis25 <104436922+JuanCelis25@users.noreply.github.com>
2022-09-15 01:31:02 -0500

    Modal5

--- js/modal.js
+++ js/modal.js
@@ -23,6 +23,7 @@ abrir2.addEventListener('click', ()=>{
       modal.classList.replace("remplazo","mo");
       modal3.classList.replace("remplazo3","mo3");
       modal4.classList.replace("remplazo4","mo4");
+      modal5.classList.replace("remplazo5","mo5");
 });
 cerrar2.addEventListener('click', ()=>{
       modal2.classList.replace('remplazo2','mo2');
@@ -39,7 +40,7 @@ abrir3.addEventListener('click', ()=>{
       modal.classList.replace("remplazo","mo");
       modal2.classList.replace("remplazo2","mo2");
       modal4.classList.replace("remplazo4","mo4");
-      
+      modal5.classList.replace("remplazo5","mo5");
 });
 cerrar3.addEventListener('click', ()=>{
       modal3.classList.replace('remplazo3','mo3');
@@ -56,8 +57,94 @@ abrir4.addEventListener('click', ()=>{
       modal.classList.replace("remplazo","mo");
       modal2.classList.replace("remplazo2","mo2");
       modal3.classList.replace("remplazo3","mo3");
+      modal5.classList.replace("remplazo5","mo5");
       
 });
 cerrar4.addEventListener('click', ()=>{
       modal4.classList.replace('remplazo4','mo4');
 });
+
+
+/*Quinto modal*/
+const abrir5 = document.getElementById('eje5');
+const modal5 = document.getElementById('mo5');
+const cerrar5 = document.getElementById('close5');
+abrir5.addEventListener('click', ()=>{
+      modal5.classList.replace("mo5","remplazo5");
+      modal.classList.replace("remplazo","mo");
+      modal2.classList.replace("remplazo2","mo2");
+      modal3.classList.replace("remplazo3","mo3");
+      modal4.classList.replace("remplazo4","mo4");
+});
+cerrar5.addEventListener('click', ()=>{
+      modal5.classList.replace('remplazo5','mo5');
+});
+
+
+
+/* Circulo*/
+const circulo = document.getElementById('cir');
+const opcion = document.getElementById('arepe');
+circulo.addEventListener('click',()=>{
+      opcion.classList.replace('re1','mostrar');
+      opcion2.classList.replace('mostrar','re2');
+      opc3.classList.replace('mostrar','op3');
+      opc4.classList.replace('mostrar','op4');
+
+      TrianguloA.classList.replace("cambi2","triArea");
+      TrianguloP.classList.replace('cambi2','triPerimetro');
+      
+      pRec.classList.replace('show3','periRec');
+      ARec.classList.replace('show3','areaRec');
+      arcu2.classList.replace('show4','periCua');
+      arcu.classList.replace('show4','areaCua');
+});
+
+/*Triangulo*/
+const triangulo = document.getElementById('tri');
+const opcion2 = document.getElementById('arepe2');
+triangulo.addEventListener('click',()=>{
+      opcion2.classList.replace('re2','mostrar');
+      opcion.classList.replace('mostrar','re1');
+      opc3.classList.replace('mostrar','op3');
+      opc4.classList.replace('mostrar','op4');
+
+      circuloA.classList.replace('cambi','cirArea');
+      circuloP.classList.replace('cambi','cirPerimetro');
+      pRec.classList.replace('show3','periRec');
+      ARec.classList.replace('show3','areaRec');
+      arcu2.classList.replace('show4','periCua');
+      arcu.classList.replace('show4','areaCua');
+      
+});
+
+/*Circulo */
+const areaCirculo = document.getElementById('areC');
+const circuloA = document.getElementById('cirArea');
+areaCirculo.addEventListener('click', ()=>{
+      circuloA.classList.replace('cirArea','cambi');
+      circuloP.classList.replace('cambi','cirPerimetro');
+});
+const periCirculo = document.getElementById('perC');
+const circuloP = document.getElementById('cirPerimetro');
+periCirculo.addEventListener('click',()=>{
+      circuloA.classList.replace('cambi','cirArea');
+      circuloP.classList.replace('cirPerimetro','cambi');
+});
+
+/*Triangulo */
+const areaTriangulo = document.getElementById('areT');
+const TrianguloA = document.getElementById('triArea');
+areaTriangulo.addEventListener('click', ()=>{
+      TrianguloA.classList.replace('triArea','cambi2');
+      TrianguloP.classList.replace('cambi2','triPerimetro');
+});
+const periTriangulo = document.getElementById('perT');
+const TrianguloP = document.getElementById('triPerimetro');
+periTriangulo.addEventListener('click',()=>{
+      TrianguloA.classList.replace('cambi2','triArea');
+      TrianguloP.classList.replace('triPerimetro','cambi2');
+});
+
+
+
